30596cb967bf3ae96305931103a44cc705d5992a
jonschlinkert <jon.schlinkert@sellside.com>
2016-04-03 15:38:24 -0400

    add "sub-schema" to example

--- example.js
+++ example.js
@@ -1,8 +1,24 @@
 'use strict';
 
 var fs = require('fs');
-var Schema = require('map-schema');
+var Schema = require('./');
 var isObject = require('isobject');
+
+function verb(config, options) {
+  var schema = new Schema(options);
+
+  schema.field('toc', ['boolean', 'object'], {
+    normalize: function(val, key, config, schema) {
+      if (typeof val === 'boolean') {
+        val = { render: val };
+      }
+      return val;
+    }
+  });
+
+  return schema;
+}
+
 
 // create a schema
 var schema = new Schema()
@@ -24,9 +40,20 @@ var schema = new Schema()
   .field('license', 'string', {
     default: 'MIT'
   })
+  .field('verb', ['object'], {
+    normalize: function(val, key, config, schema) {
+      if (typeof val !== 'undefined') {
+        var schema = verb(schema.options);
+        val = config[key] = schema.normalize(val);
+        console.log(val)
+        return val;
+      }
+    }
+  })
 
 var pkg = require('./package');
 // normalize an object
-console.log(schema.normalize(pkg));
+var res = schema.normalize(pkg);
+// console.log(res);
 // validation errors array
-console.log(schema.errors);
+// console.log(schema.warnings);
